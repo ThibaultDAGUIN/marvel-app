50f5cc73f342d17548311050960fa078f58da0ea
Thibault DAGUIN <daguint@gmail.com>
2024-09-30 17:53:17 +0200

    ✨ Create CharactersList component

--- src/App.jsx
+++ src/App.jsx
@@ -39,31 +39,16 @@ export default App;
 
 
 import './App.css'
+import { CharactersList } from './components/CharactersList'
 import characters from './data/characters.json'
+
+
 
 function App() {
   return (
     <>
       <h1>Marvel Characters</h1>
-      <ul id="characters">
-        {characters.map((character) => (
-          <li key={character.id}>
-            {character.name}
-          </li>
-        ))}
-
-        {/* <li>
-          Beast
-        </li>
-        <li>
-          Captain America
-        </li>
-        <li>
-          Deadpool
-        </li> */}
-
-        
-      </ul>
+      <CharactersList characters={characters}/>
     </>
   )
 }
