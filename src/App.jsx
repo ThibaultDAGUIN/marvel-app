93c8d7bb3f302fb3cb02073b4b6176a2a10bc94e
Thibault DAGUIN <daguint@gmail.com>
2024-09-30 17:35:14 +0200

    Comments on dynamic list for characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,43 @@
+// Hello avec React.createElement("h1",...)
+
+/*
+import React from "react";
+function App() {
+  return React.createElement("h1", {}, "Hello World");
+}
+export default App;
+*/
+
+// Hello avec balise h1
+
+// function App() {
+//   return (<h1>Hello World from react with JSX</h1>);
+// }
+// export default App;
+
+// Petite app
+
+// function Title({ color = 'red', children, hidden = false, ...props }) {
+//   if (hidden) {
+//       return null;
+//   }
+//   return (<h1 style={{color: color}} {...props}>{children}</h1>);
+// }
+// function App() {
+// return (
+//   <>
+//     {/* 
+//         La propriété id et data-demo sont passées au composant Title grâce au spread operator.
+//         La propriété color est définie dans le composant Title
+//      */}
+//     <Title color="blue" id="my-id" data-demo="demo">Hello World</Title>
+//   </>
+// );
+// }
+// export default App;
+
+
+
 import './App.css'
 import characters from './data/characters.json'
 
